7ed0fb8f2291909c6409550453c21da9daad2e69
Steven Zuluaga <steven000069@hotmail.com>
2022-12-05 04:16:52 -0500

    Ultimos ajustes api

--- app/main.py
+++ app/main.py
@@ -58,6 +58,8 @@ class Employee(BaseModel):
     MaritalStatusMarried:float
     MaritalStatusSingle:float
 
+with open('./app/model.pkl','rb') as f:
+    model=pickle.load(f)
 
 @app.get("/")
 def root():
@@ -66,104 +68,8 @@ def root():
 
 @app.post("/predict_employee")
 def predict(employee:Employee):
-    age=employee.Age
-    bussinessTravel=employee.BusinessTravel
-    DailyRate=employee.DailyRate
-    DistanceFromHome=employee.DistanceFromHome
-    Education=employee.Education
-    EnvironmentSatisfaction=employee.EnvironmentSatisfaction
-    Gender=employee.Gender
-    HourlyRate=employee.HourlyRate
-    JobInvolvement=employee.JobInvolvement
-    JobLevel=employee.JobLevel
-    JobSatisfaction=employee.JobSatisfaction
-    MonthlyIncome=employee.MonthlyIncome
-    MonthlyRate=employee.MonthlyRate
-    NumCompaniesWorked=employee.NumCompaniesWorked
-    OverTime=employee.OverTime
-    PercentSalaryHike=employee.PercentSalaryHike
-    PerformanceRating=employee.PerformanceRating
-    RelationshipSatisfaction=employee.RelationshipSatisfaction
-    StockOptionLevel=employee.StockOptionLevel
-    TotalWorkingYears=employee.TotalWorkingYears
-    TrainingTimesLastYear=employee.TrainingTimesLastYear
-    WorkLifeBalance=employee.WorkLifeBalance
-    YearsAtCompany=employee.YearsAtCompany
-    YearsInCurrentRole=employee.YearsInCurrentRole
-    YearsSinceLastPromotion=employee.YearsSinceLastPromotion
-    YearsWithCurrManager=employee.YearsWithCurrManager
-    Department_HumanResources=employee.DepartmentHumanResources
-    Department_ResearchDevelopment=employee.DepartmentResearchDevelopment
-    Department_Sales=employee.DepartmentSales
-    EducationField_HumanResources=employee.EducationFieldHumanResources
-    EducationField_LifeSciences=employee.EducationFieldLifeSciences
-    EducationField_Marketing=employee.EducationFieldMarketing
-    EducationField_Medical=employee.EducationFieldMedical
-    EducationField_Other=employee.EducationFieldOther
-    EducationField_TechnicalDegree=employee.EducationFieldTechnicalDegree
-    JobRole_HealthcareRepresentative=employee.JobRoleHealthcareRepresentative
-    JobRole_HumanResources=employee.JobRoleHumanResources
-    JobRole_LaboratoryTechnician=employee.JobRoleLaboratoryTechnician
-    JobRole_Manager=employee.JobRoleManager
-    JobRole_ManufacturingDirector=employee.JobRoleManufacturingDirector
-    JobRole_ResearchDirector=employee.JobRoleResearchDirector
-    JobRole_ResearchScientist=employee.JobRoleResearchScientist
-    JobRole_SalesExecutive=employee.JobRoleSalesExecutive
-    JobRole_SalesRepresentative=employee.JobRoleSalesRepresentative
-    MaritalStatus_Divorced=employee.MaritalStatusDivorced
-    MaritalStatus_Married=employee.MaritalStatusMarried
-    MaritalStatus_Single=employee.MaritalStatusSingle
+    df=pd.DataFrame([employee.dict().values()],columns=employee.dict().keys())
 
-    features=[[age,bussinessTravel,DailyRate,
-DistanceFromHome,
-Education,
-EnvironmentSatisfaction,
-Gender,
-HourlyRate,
-JobInvolvement,
-JobLevel,
-JobSatisfaction,
-MonthlyIncome,
-MonthlyRate,
-NumCompaniesWorked,
-OverTime,
-PercentSalaryHike,
-PerformanceRating,
-RelationshipSatisfaction,
-StockOptionLevel,
-TotalWorkingYears,
-TrainingTimesLastYear,
-WorkLifeBalance,
-YearsAtCompany,
-YearsInCurrentRole,
-YearsSinceLastPromotion,
-YearsWithCurrManager,
-Department_HumanResources,
-Department_ResearchDevelopment,
-Department_Sales,
-EducationField_HumanResources,
-EducationField_LifeSciences,
-EducationField_Marketing,
-EducationField_Medical,
-EducationField_Other,
-EducationField_TechnicalDegree,
-JobRole_HealthcareRepresentative,
-JobRole_HumanResources,
-JobRole_LaboratoryTechnician,
-JobRole_Manager,
-JobRole_ManufacturingDirector,
-JobRole_ResearchDirector,
-JobRole_ResearchScientist,
-JobRole_SalesExecutive,
-JobRole_SalesRepresentative,
-MaritalStatus_Divorced,
-MaritalStatus_Married,
-MaritalStatus_Single]]
-    
-    
-    
-    modelo=pickle.load(open('./app/model.pkl', 'rb'))
-    pred=modelo.predict(features)
-    return pred
-
-
+    # modelo=pickle.load(open('./app/model.pkl', 'rb'))
+    pred=model.predict(df)
+    return {"prediction" :int(pred)}
